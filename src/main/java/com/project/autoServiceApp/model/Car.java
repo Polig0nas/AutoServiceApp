3029359532a1e342a86db96c3d660be922ffe230
Polig0nas <t.polegenko@gmail.com>
2023-02-14 07:54:40 +0200

    replacing @Data annotation bcz suggested

--- src/main/java/com/project/autoServiceApp/model/Car.java
+++ src/main/java/com/project/autoServiceApp/model/Car.java
@@ -1,10 +1,20 @@
 package com.project.autoServiceApp.model;
 
 import lombok.*;
-import javax.persistence.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity(name = "Car")
 public class Car {
     @Id
@@ -16,4 +26,17 @@ public class Car {
     private String yearOfMade;
     private String bodyType;
     private String fuelType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Car car = (Car) o;
+        return id != null && Objects.equals(id, car.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
