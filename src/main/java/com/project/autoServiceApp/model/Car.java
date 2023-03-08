2f413d56a833c932570a5c00c6a212ec20ab966b
Polig0nas <t.polegenko@gmail.com>
2023-03-08 19:50:23 +0200

    Car class commit

--- src/main/java/com/project/autoServiceApp/model/Car.java
+++ src/main/java/com/project/autoServiceApp/model/Car.java
@@ -3,39 +3,27 @@ package com.project.autoServiceApp.model;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@Entity(name = "Car")
+@Data
+@Entity(name = "CAR")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "LICENCE_PLATE")
     private String licencePlate;
+    @Column(name = "MAKE")
     private String make;
+    @Column(name = "MODEL")
     private String model;
+    @Column(name = "YEAR_OF_MADE")
     private String yearOfMade;
+    @Column(name = "BODY_TYPE")
     private String bodyType;
+    @Column(name = "FUEL_TYPE")
     private String fuelType;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Car car = (Car) o;
-        return id != null && Objects.equals(id, car.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
